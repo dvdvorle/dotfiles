c1f980efbc4ad6eede230703069bd4a21af841ae
Dominique van de Vorle <Dominique.van.de.Vorle@wigo4it.nl>
2021-08-08 19:58:10 +0200

    WSL provisioning script toegevoegd

--- README.md
+++ README.md
@@ -18,13 +18,17 @@ Sla beide exports op buiten de VDI, bijv. je H:\ schijf
 
 1. Edit in `home\.gitconfig` de user.email en user.name
 2. Open powershell als Administrator, navigeer naar de huidige map en draai de volgende commando's
-
-```powershell
-  Set-ExecutionPolicy RemoteSigned
-  Unblock-File .\provision1.ps1
-  .\provision1.ps1
-```
-
+   ```powershell
+   Set-ExecutionPolicy RemoteSigned
+   Unblock-File .\provision1.ps1
+   .\provision1.ps1
+   ```
 3. Wacht tot het script is afgelopen, de computer wordt automatisch herstart.
-4. Log opnieuw in. 
+4. Log opnieuw in. Automatisch wordt het vervolg, `provision2.ps1`, gestart.
 5. Enkele resterende handmatige acties worden getoond aan het einde van `provision2.ps1`
+6. Start Ubuntu, creeer een account, navigeer naar de map met scripts (bijv. `/mnt/d/dotfiles-main/`) en voer de volgende commando's uit
+   ```bash
+   cp provision.sh ~/provision.sh
+   cd ~
+   sudo ./provision.sh
+   ```
---
+++ provision.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Remount disks to prevent
+umount /mnt/c
+mount -t drvfs C: /mnt/c -o metadata
+
+umount /mnt/d
+mount -t drvfs D: /mnt/d -o metadata
+
+
+# Install Git Credential Manager Core
+curl -L https://github.com/microsoft/Git-Credential-Manager-Core/releases/download/v2.0.475/gcmcore-linux_amd64.2.0.475.64295.deb --output gcmcore-linux_amd64.deb
+dpkg -i gcmcore-linux_amd64.deb
+git-credential-manager-core configure
+
+# Configure GCM
+git config --global --replace-all credential.https://dev.azure.com.usehttppath true
+git config --global credential.credentialStore cache
+
+# Install Azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+
+# Install Helm
+curl https://baltocdn.com/helm/signing.asc | apt-key add -
+apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+apt-get update && apt-get install helm -y
+
+
+# Install terraform
+apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update && apt-get install terraform
